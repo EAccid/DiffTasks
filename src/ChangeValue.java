5db118382db8a1ef57cdb4339091098d03cee901
EAccid <elena.briginets@gmail.com>
2016-11-17 12:57:07 +0200

    diff

---
+++ src/ChangeValue.java
@@ -0,0 +1,26 @@
+public class ChangeValue {
+
+    public static void main(String[] args) {
+
+        int a = -5;
+        int b = 20;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+        a = a * b;
+        b = a / b;
+        a = a / b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+}
